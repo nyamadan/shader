d2b65652932dee7325d5adc77ab01c66635120c9
Yoshiki YAMADA <nyamadandan@gmail.com>
2020-04-06 22:33:17 +0900

    fix snippet

---
+++ shader/scale.frag.glsl
@@ -0,0 +1,28 @@
+#version 310 es
+
+precision mediump float;
+
+#extension GL_GOOGLE_include_directive : enable
+
+#include "./lib/uniforms.glsl"
+
+float circle(vec2 position, float radius) {
+  return step(radius, length(position - vec2(0.5)));
+}
+
+mat2 scale(vec2 scale) { return mat2(scale.x, 0.0, 0.0, scale.y); }
+
+layout(location = 0) out vec4 fragColor;
+
+void main() {
+  vec2 coord = gl_FragCoord.xy / Resolution;
+  vec3 color = vec3(0.0);
+
+  coord -= vec2(0.5);
+  coord = scale(vec2(sin(Time) + 2.0)) * coord;
+  coord += vec2(0.5);
+
+  color = vec3(circle(coord, 0.3));
+
+  fragColor = vec4(color, 1.0);
+}
