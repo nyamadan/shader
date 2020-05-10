ea9139435861bfd24ffe9e84cfee5166a80857a7
Yoshiki YAMADA <nyamadandan@gmail.com>
2020-05-10 22:31:49 +0900

    fix for 100

--- shader/lib/raymarching.glsl
+++ shader/lib/raymarching.glsl
@@ -6,21 +6,15 @@ struct HitResult {
   float d;
 };
 
-HitResult map(in vec3 pos);
+HitResult map(in vec3);
 
 HitResult opU(HitResult r1, HitResult r2) { return (r1.d < r2.d) ? r1 : r2; }
 
 // http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
 vec3 calcNormal(in vec3 pos) {
-  // inspired by tdhooper and klems - a way to prevent the compiler from
-  // inlining map() 4 times
-  vec3 n = vec3(0.0);
-  for (int i = 0; i < 4; i++) {
-    vec3 e = 0.5773 *
-             (2.0 * vec3((((i + 3) >> 1) & 1), ((i >> 1) & 1), (i & 1)) - 1.0);
-    n += e * map(pos + 0.0005 * e).d;
-  }
-  return normalize(n);
+  vec2 e = vec2(1.0, -1.0) * 0.5773 * 0.0005;
+  return normalize(e.xyy * map(pos + e.xyy).d + e.yyx * map(pos + e.yyx).d +
+                   e.yxy * map(pos + e.yxy).d + e.xxx * map(pos + e.xxx).d);
 }
 
 // http://iquilezles.org/www/articles/rmshadows/rmshadows.htm
--- shader/raymarching.frag.glsl
+++ shader/raymarching.frag.glsl
@@ -4,10 +4,10 @@ precision highp float;
 
 #extension GL_GOOGLE_include_directive : enable
 
-#include "./lib/uniforms.glsl"
-#include "./lib/hsb.glsl"
-#include "./lib/raymarching.glsl"
+layout(location = 0) uniform vec2 resolution;
+
 #include "./lib/distance_functions.glsl"
+#include "./lib/raymarching.glsl"
 
 // materials
 const int MATERIAL_EMPTY = 0;
@@ -17,24 +17,29 @@ layout(location = 0) out vec4 fragColor;
 
 HitResult map(in vec3 pos) {
   HitResult res = HitResult(MATERIAL_EMPTY, 1e+10);
-  res = opU(res, HitResult(MATERIAL_SPHERE, sdSphere(pos - vec3(0.0, 1.0, 0.0), 0.5)));
+  res = opU(
+      res, HitResult(MATERIAL_SPHERE, sdPlane(pos - vec3(0.0, -1.0, 0.0),
+                                              vec4(vec3(0.0, 1.0, 0.0), 0.0))));
   res = opU(res, HitResult(MATERIAL_SPHERE, sdBox(pos, vec3(0.5))));
-  res = opU(res, HitResult(MATERIAL_SPHERE, sdPlane(pos - vec3(0.0, -1.0, 0.0), vec4(vec3(0.0, 1.0, 0.0), 0.0))));
+  res = opU(res, HitResult(MATERIAL_SPHERE,
+                           sdSphere(pos - vec3(0.0, 1.0, 0.0), 0.5)));
   return res;
 }
 
 void main() {
-  const int MAX_STEP = 512;
+  const int MAX_STEP = 100;
   const float EPS = 1e-6;
   float fov = 30.0;
 
   vec2 p =
       (2.0 * gl_FragCoord.xy - resolution.xy) / min(resolution.x, resolution.y);
 
-  vec3 center = vec3(0.0, 0.0, 0.0);
   vec3 up = vec3(0.0, 1.0, 0.0);
-  vec3 ro = vec3(0.0, 5.0, 5.0);
-  vec3 rd = getRay(p, mat3(lookAt(ro, center, up)), fov);
+  vec3 center = vec3(0.0, 0.0, 0.0);
+
+  vec3 ro = vec3(5.0, 6.0, 7.0);
+  mat4 mView = lookAt(ro, center, up);
+  vec3 rd = getRay(p, mat3(mView), fov);
   vec3 color = vec3(0.0);
 
   float depth = 0.0;
@@ -44,16 +49,13 @@ void main() {
     HitResult res = map(rayPos);
 
     if (res.d < EPS) {
-      switch (res.m) {
-      case MATERIAL_SPHERE:
+      if (res.m == MATERIAL_SPHERE) {
         vec3 nor = calcNormal(rayPos);
         vec3 lightDir = normalize(vec3(1.0, 2.0, 3.0));
         float diff = max(dot(lightDir, nor), 0.0);
         color = vec3(0.5 * diff * diff + 0.5);
         break;
       }
-
-      break;
     }
 
     depth += res.d;
